efc73b2684524e0b8c38980616a4a3aa41fc1349
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-11-09 17:47:24 +0100

    s

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,68 @@
 import { useDispatch } from "react-redux";
 import css from "./Contact.module.css";
-import { deleteContacts } from "redux/contactsSlice";
+import { deleteContacts, editContacts } from "redux/contactsSlice";
+import { useEffect, useState } from "react";
 
 const Contact = ({ contacts }) => {
   const dispatch = useDispatch();
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [name, setName] = useState(contacts.name);
+  const [number, setNumber] = useState(contacts.number);
+
+  const handlEdit = () => {
+    setIsEditMode((prev) => !prev);
+  };
+  const handleDelete = () => dispatch(deleteContacts(contacts.id));
+
+  useEffect(() => {
+    if (!isEditMode && (name !== contacts.name || number !== contacts.number)) {
+      // handlEditWord({
+      // id: word.id,
+      // ukWord,
+      // enWord,
+      // });
+      dispatch(editContacts({ id: contacts.id, name, number }));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isEditMode]);
+
+  const handleInputChange = ({ target }) => {
+    if (target.name === "editName") {
+      setName(target.value);
+      return;
+    }
+    setNumber(target.value);
+  };
   return (
     <li className={css.item}>
-      <h3>{contacts.name}</h3>
-      <h3>{contacts.number}</h3>
-
+      {!isEditMode ? (
+        <div className={css.nameNumber}>
+          <h3 className={css.name}>{contacts.name}</h3>
+          <h3 className={css.number}>{contacts.number}</h3>
+        </div>
+      ) : (
+        <>
+          <input
+            onChange={handleInputChange}
+            type="text"
+            name="editName"
+            value={name}
+          />
+          <input
+            onChange={handleInputChange}
+            type="text"
+            name="editNumber"
+            value={number}
+          />
+        </>
+      )}
+      <button onClick={handlEdit}>{isEditMode ? "Save" : "Edit"}</button>
       <button
         type="button"
         className={css.btnClose}
         aria-label="Close"
-        onClick={() => dispatch(deleteContacts(contacts.id))}
+        onClick={handleDelete}
+        sx={{ m: 1, width: "110px" }}
       >
         Delete
       </button>
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -5,18 +5,21 @@ export const contactsSlice = createSlice({
   initialState: contactslist,
   reducers: {
     createContacts: (state, action) => {
-      // console.log(action);
-      // console.log(state);
-      // console.log(action.payload);
-
       state.push(action.payload);
     },
     deleteContacts: (state, action) => {
       return state.filter(el => el.id !== action.payload);
     },
+    editContacts: (state, action) => {
+      const index = state.findIndex(
+        contacts => contacts.id === action.payload.id
+      );
+      state.splice(index, 1, action.payload);
+    },
   },
 });
 
-export const { createContacts, deleteContacts } = contactsSlice.actions;
+export const { createContacts, deleteContacts, editContacts } =
+  contactsSlice.actions;
 
 export const contactsReducer = contactsSlice.reducer;
